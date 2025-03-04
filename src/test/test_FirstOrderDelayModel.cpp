832cf1df2612efaf5f8e5c292840718b099532bf
John So <john.so@mda.space>
2025-03-04 20:12:29 +0000

    CIP-3426 added a CSV dump test case to retain the old unit test. Fix evaluation of polynomials.

--- src/test/test_FirstOrderDelayModel.cpp
+++ src/test/test_FirstOrderDelayModel.cpp
@@ -14,7 +14,7 @@ using namespace ska_mid_cbf_fodm_gen;
 * Description	:
 *       save FO poly into a cvs file - DEBUG only
 */
-void save_to_file( std::string file_name, 
+void save_to_file( const std::string& file_name, 
                    const std::vector<double>& fo_t_start, 
                    const std::vector<long double>& fo_poly, 
                    int num_fo_poly )
@@ -24,8 +24,8 @@ void save_to_file( std::string file_name,
     {
         for (int i = 0; i < num_fo_poly; i++)
         {
-            myfile << std::fixed << std::setprecision(16) << fo_t_start[i] << ",";
-            myfile << std::fixed << std::setprecision(16) << fo_t_start[i+1]<< ","; 
+            myfile << std::fixed << std::setprecision(12) << fo_t_start[i] << ",";
+            myfile << std::fixed << std::setprecision(12) << fo_t_start[i+1]<< ","; 
             myfile << std::fixed << std::setprecision(16) << fo_poly[i*2] << ","; 
             myfile << std::fixed << std::setprecision(16) << fo_poly[i*2+1] << "\n"; 
         }
@@ -34,13 +34,13 @@ void save_to_file( std::string file_name,
     myfile.close();
 }
 
-long double polyval(const double* ho_poly, int num_ho_coeff, double x)
+long double polyval(const double* ho_poly, int num_ho_coeff, double t)
 {
     using namespace boost::multiprecision;
     cpp_bin_float_50 y = ho_poly[0];
     for (int ii = 1; ii < num_ho_coeff; ii++) 
     {
-        y *= x;
+        y *= t;
         y += ho_poly[ii];
     }
     return static_cast<long double>(y);
@@ -58,8 +58,8 @@ protected:
     const int NUM_TEST_POINTS = 2000;
 
     // TBD: what thresholds to use?
-    const long double MAX_ABS_ERR = 1.0e-8;
-    const long double MAX_CUMULATED_ERR = 1.0e-7;
+    const long double MAX_ABS_ERR = 5.0e-9;
+    const long double MAX_CUMULATED_ERR = 5.0e-8;
 
     FirstOrderDelayModelTest()
     {
@@ -75,32 +75,33 @@ protected:
         return int( (t - t_fo_poly_[0]) / fo_poly_interval);
     }
 
-    void lsq_fit_max_error_test_common(const double* ho_poly, double fo_poly_interval) {
+    void lsq_fit_max_error_test_common(const double* ho_poly, double fo_poly_interval, bool dump_csv = false) {
         // Generate num_fo_polys FODMs from the start of HODM
-        double t_start = ho_poly[0];
-        double t_stop = ho_poly[1];
+        double hodm_t_start = ho_poly[0];
+        double hodm_t_stop = ho_poly[1];
         for (int ii = 0 ; ii < NUM_FO_POLYS+1; ii++) 
         {
-            t_fo_poly_[ii] = t_start + fo_poly_interval * ii;
+            t_fo_poly_[ii] = hodm_t_start + fo_poly_interval * ii;
         }
         
         // LSQ fit
         FirstOrderDelayModel test_model;
-        bool result = test_model.process(t_start, t_stop, NUM_HO_COEFF, (ho_poly+2), NUM_LSQ_POINTS, NUM_FO_POLYS, t_fo_poly_, fo_polys_);
+        bool result = test_model.process(hodm_t_start, hodm_t_stop, NUM_HO_COEFF, (ho_poly+2), NUM_LSQ_POINTS, NUM_FO_POLYS, t_fo_poly_, fo_polys_);
         EXPECT_TRUE(result);
 
         // Sample random points between within the range of generated FO polynomials
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_real_distribution<> dis(t_fo_poly_[0], t_fo_poly_[NUM_FO_POLYS]);
+
         long double max_abs_err = 0.0;   // Checks for max error of FO fit against HO poly at any single point 
-        long double cumulated_err = 0.0; // Checks for fitting bias
+        long double cumulated_err = 0.0; // Checks for fitting bias. Ideally should be close to 0.
         for (int ii = 0; ii < NUM_TEST_POINTS; ii++)
         {
             double t = dis(gen);
-            long double val_from_ho = polyval((ho_poly+2), NUM_HO_COEFF, t);
             int fo_idx = find_fo_poly_idx(t, fo_poly_interval);
             ASSERT_LT(fo_idx, t_fo_poly_.size());
+            long double val_from_ho = polyval((ho_poly+2), NUM_HO_COEFF, t - hodm_t_start);
             long double val_from_fo = fo_polys_[fo_idx*2+1] + fo_polys_[fo_idx*2] * (t - t_fo_poly_[fo_idx]);
             long double diff = val_from_ho - val_from_fo;
             max_abs_err = abs(diff) > max_abs_err ? abs(diff) : max_abs_err;
@@ -109,6 +110,12 @@ protected:
 
         EXPECT_LT(max_abs_err, MAX_ABS_ERR);
         EXPECT_LT(abs(cumulated_err), MAX_CUMULATED_ERR);
+
+        if (dump_csv)
+        {
+            std::string csv_file("FOTCP_SRC1_Receptor1_polX.csv");
+            save_to_file(csv_file, t_fo_poly_, fo_polys_, NUM_FO_POLYS);
+        }
     }
 
     std::vector<double> t_fo_poly_;
@@ -117,6 +124,17 @@ protected:
 
 // Test with a HODM generated from MATLAB
 TEST_F(FirstOrderDelayModelTest, LsqFitMaxErrorTest)
+{
+    double ho_poly[HO_POLY_LEN] = {
+        1.0000000000000E+01,3.0000000000000E+01,4.513184775273619937E-17,3.016563864250689452E-14,
+        1.077965332504251907E-09,-7.680455181115336256E-05,-1.216193871021531203E+00,28887.4980 };
+    double fo_poly_interval = 1.0 / 128;
+
+    lsq_fit_max_error_test_common(ho_poly, fo_poly_interval);
+}
+
+// Test with a HODM that starts at t = 0.
+TEST_F(FirstOrderDelayModelTest, HodmStart0MaxErrorTest)
 {
     double ho_poly[HO_POLY_LEN] = {
         0.0000000000000E+00,3.0000000000000E+01,4.513184775273619937E-17,3.016563864250689452E-14,
@@ -130,7 +148,7 @@ TEST_F(FirstOrderDelayModelTest, LsqFitMaxErrorTest)
 TEST_F(FirstOrderDelayModelTest, ConstHodmLsqFitMaxErrorTest)
 {
     double ho_poly[HO_POLY_LEN] = {
-        0.0000000000000E+00,3.0000000000000E+01,0.0000000000000E+00,0.0000000000000E+00,
+        1.0000000000000E+01,3.0000000000000E+01,0.0000000000000E+00,0.0000000000000E+00,
         0.0000000000000E+00,0.0000000000000E+00,0.0000000000000E+00,28887.4980 };
     double fo_poly_interval = 1.0 / 128;
 
@@ -157,15 +175,15 @@ TEST_F(FirstOrderDelayModelTest, BeyondDurationTest)
         1.077965332504251907E-09,-7.680455181115336256E-05,-1.216193871021531203E+00,28887.4980 };
     double fo_poly_interval = 0.02;
 
-    double t_start = ho_poly[0];
-    double t_stop = ho_poly[1];
+    double hodm_t_start = ho_poly[0];
+    double hodm_t_stop = ho_poly[1];
     for (int ii = 0 ; ii < NUM_FO_POLYS+1; ii++) 
     {
-        t_fo_poly_[ii] = t_start + fo_poly_interval * ii;
+        t_fo_poly_[ii] = hodm_t_start + fo_poly_interval * ii;
     }
     
     FirstOrderDelayModel test_model;
-    bool result = test_model.process(t_start, t_stop, NUM_HO_COEFF, (ho_poly+2), NUM_LSQ_POINTS, NUM_FO_POLYS, t_fo_poly_, fo_polys_);
+    bool result = test_model.process(hodm_t_start, hodm_t_stop, NUM_HO_COEFF, (ho_poly+2), NUM_LSQ_POINTS, NUM_FO_POLYS, t_fo_poly_, fo_polys_);
     EXPECT_FALSE(result);
 }
 
@@ -178,8 +196,8 @@ TEST_F(FirstOrderDelayModelTest, BeforeStartTimeTest)
         1.077965332504251907E-09,-7.680455181115336256E-05,-1.216193871021531203E+00,28887.4980 };
     double fo_poly_interval = 0.01;
 
-    double t_start = ho_poly[0];
-    double t_stop = ho_poly[1];
+    double hodm_t_start = ho_poly[0];
+    double hodm_t_stop = ho_poly[1];
     double fodm_start = 45.0; // FODM starts at t = 45
     for (int ii = 0 ; ii < NUM_FO_POLYS+1; ii++) 
     {
@@ -187,6 +205,16 @@ TEST_F(FirstOrderDelayModelTest, BeforeStartTimeTest)
     }
     
     FirstOrderDelayModel test_model;
-    bool result = test_model.process(t_start, t_stop, NUM_HO_COEFF, (ho_poly+2), NUM_LSQ_POINTS, NUM_FO_POLYS, t_fo_poly_, fo_polys_);
+    bool result = test_model.process(hodm_t_start, hodm_t_stop, NUM_HO_COEFF, (ho_poly+2), NUM_LSQ_POINTS, NUM_FO_POLYS, t_fo_poly_, fo_polys_);
     EXPECT_FALSE(result);
+}
+
+// This test additionally dumps the FODMs to a CSV file, the MATLAB/fit.m can read
+// the CSV file, compute and plot the errors vs HODM.
+TEST_F(FirstOrderDelayModelTest, DumpToFileTest)
+{
+    double ho_poly[HO_POLY_LEN] = {27.0, 37.0, -0.0000000000000777454450817839, 0.0000000000013703522293644218,-0.0000000010937608165606331000, 0.0000767171471221868880000000, 1.2203389596567424000000000000, -28854.6047841830330000000000000000};
+    double fo_poly_interval = 0.01;
+
+    lsq_fit_max_error_test_common(ho_poly, fo_poly_interval, true);
 }
